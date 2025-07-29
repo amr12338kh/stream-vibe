15aa6df2e6ce6c6c5ae8c0a4e67c140a55dae9ca
Amr <amrkhaled12338@gmail.com>
2025-07-29 18:34:30 +0300

    enhance header

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -14,6 +14,8 @@ const Header = () => {
   const shouldUsePaddingX =
     pathname.startsWith("/movies") || pathname.startsWith("/search");
 
+  const isSearchPage = pathname === "/search";
+
   return (
     <>
       <div className=" fixed top-0 z-40 w-full h-20 bg-linear-to-b from-black-8 via-black-10/50 to-transparent" />
@@ -29,13 +31,17 @@ const Header = () => {
           </div>
           <div className="items-center gap-5 hidden md:flex">
             <Link href={"/search"}>
-              <Search className=" size-[18px] cursor-pointer" />
+              <Search
+                className={`size-[18px] cursor-pointer ${isSearchPage && "text-primary"} hover:text-primary transition-colors duration-200`}
+              />
             </Link>
-            <Bell className=" size-[18px] cursor-pointer" />
+            <Bell className=" size-[18px] cursor-pointer hover:text-primary transition-colors duration-200" />
           </div>
           <div className="md:hidden flex items-center gap-4 bg-black-10 py-2 px-3 rounded-md">
             <Link href={"/search"}>
-              <Search className=" size-[18px] cursor-pointer" />
+              <Search
+                className={`size-[18px] cursor-pointer ${isSearchPage && "text-primary"} hover:text-primary transition-colors duration-200`}
+              />
             </Link>
             <div className="w-px h-5 bg-white opacity-50" />
             <Sidebar />
